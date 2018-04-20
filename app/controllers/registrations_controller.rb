c2297216cdad554d45c675a42ad661f072efd441
Caleb Baum <lorencalebbaum@cedarville.edu>
2018-04-20 14:11:31 -0400

    initial commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+    before_action :update_sanitized_params
+ 
+    def update_sanitized_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :email, :password, :password_confirmation])    
+     end
+ end
+ 
